266b7aa72f492cb25b1da55a75bb4e4b9e62c112
Nishika Bajaj <nb101604@gmail.com>
2024-12-17 18:17:12 -0600

    Set landing page w/ BG image, logo, and buttons

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -10,28 +10,34 @@ Recall:
 */
 
 import React from 'react'; // Creates object called React, which contains necessary methods to use React library, necessary in App.js and index.js
-import logo from './logo.svg';
+import logo from './logo0.png'
 import './App.css';
+
+
+function Button(props){
+  return(
+    <button>{props.text}</button>
+  );
+}
 
 function App() { // function component, can be used to create as many instances of itself as needed, must start with capital
   return (
     <div className="App">
+      
+      <head>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+        </style>
+      </head>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button text="Log In"/>
+        <Button text="Sign Up"/>
       </header>
     </div>
-  );
-}
+    );
+  }
+
 
 export default App; // is exported to index.js so it can be rendered at the entry point, must be imported in index.js
